dbb062795f8cc984d081a74f25228996dee33039
William Crépault <william.crepault@gmail.com>
2024-04-05 13:02:50 -0400

    récupérations des liens "script src" dans les bas de page

--- Nicolas_Plouffe_William_Crepault_TP2_Web_2/foots.infos.php
+++ Nicolas_Plouffe_William_Crepault_TP2_Web_2/foots.infos.php
@@ -15,7 +15,11 @@
 </footer>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 <script src="java_script/highlightBtn.js"></script>
-<script src="java_script/header_nav_footer.js"></script>
+<script src="java_script/date.js"></script>
+<script src="java_script/jsonList.js"></script>
+<script src="java_script/validationAlbumjQuery.js"></script>
+<script src="java_script/validationArtisteJS.js"></script>
+<script src="java_script/validationOeuvre.js"></script>
 </body>
 
 </html>
