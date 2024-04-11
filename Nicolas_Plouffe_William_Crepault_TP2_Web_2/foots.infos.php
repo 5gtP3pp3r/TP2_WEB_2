c810e90a6f4641cd6f291e0ccef0f0f0acbbeb6b
William Crépault <william.crepault@gmail.com>
2024-04-11 08:30:20 -0400

    changement index html vers php + modifications code foots.infos.php

--- Nicolas_Plouffe_William_Crepault_TP2_Web_2/foots.infos.php
+++ Nicolas_Plouffe_William_Crepault_TP2_Web_2/foots.infos.php
@@ -13,13 +13,40 @@
         </div>
     </div>
 </footer>
+<?php
+
+    $pageScript = GetScript($pageName);
+
+    function GetScript($pageName)
+    {
+        if ($pageName == 'pageAcceuil')
+        {
+            return '<script src="java_script/highlightBtn.js"></script>';
+        }
+        else if ($pageName == 'pageListeAlbums')
+        {
+            return '<script src="java_script/jsonList.js"></script>';
+        }
+        else if ($pageName == 'pageArtiste')
+        {
+            return '<script src="java_script/validationArtisteJS.js"></script>';
+        }
+        else if ($pageName == 'pageOeuvre')
+        {
+            return '<script src="java_script/date.js"></script>' . 
+                   '<script src="java_script/validationOeuvre.js"></script>';
+        }
+        else if ($pageName == 'pageAlbum')
+        {
+            return '<script src="java_script/date.js"></script>' . 
+                   '<script src="java_script/validationAlbumjQuery.js"></script>';
+        }
+    }   
+?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-<script src="java_script/highlightBtn.js"></script>
-<script src="java_script/date.js"></script>
-<script src="java_script/jsonList.js"></script>
-<script src="java_script/validationAlbumjQuery.js"></script>
-<script src="java_script/validationArtisteJS.js"></script>
-<script src="java_script/validationOeuvre.js"></script>
+<?php
+    echo $pageScript;
+?>
 </body>
 
 </html>
