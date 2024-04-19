2fdeca389cbc9d59bda8f7495ee586cb2641219a
William Crépault <william.crepault@gmail.com>
2024-04-19 17:50:20 -0400

    position btn "pageAchat"

--- Nicolas_Plouffe_William_Crepault_TP2_Web_2/pageAchat.php
+++ Nicolas_Plouffe_William_Crepault_TP2_Web_2/pageAchat.php
@@ -15,7 +15,7 @@
             <div class="col-sm-12 col-lg-6">
                 <div class="custom-border px-2">
                     <h3 id="menu">Voir mon panier:&nbsp;&nbsp;</h3>
-                    <div class="col-sm-12 d-flex justify-content-end">   
+                    <div class="col-sm-12 d-flex justify-content-end px-4 py-2">   
                         <?php    
                         $qt = 1;                                                
                         echo '<button type="button" id="deconnetion" class="styled-button"><img src="Images/panier.png" alt="panier">&nbsp;&nbsp;'.$qt.' article(s)</button>'
