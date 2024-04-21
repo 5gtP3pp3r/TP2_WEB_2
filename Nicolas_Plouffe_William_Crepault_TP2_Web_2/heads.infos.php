99821e1a0248d12643216463055c83a59538697e
William Crépault <william.crepault@gmail.com>
2024-04-21 04:07:21 -0400

    mise au point pageAchat.pnp liste fonctionnelles

--- Nicolas_Plouffe_William_Crepault_TP2_Web_2/heads.infos.php
+++ Nicolas_Plouffe_William_Crepault_TP2_Web_2/heads.infos.php
@@ -1,8 +1,7 @@
-<?php 
-    if (session_status() == PHP_SESSION_NONE) 
-    {
-        session_start();
-    }
+<?php
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 ?>
 
 <!DOCTYPE html>
@@ -37,21 +36,19 @@
             </div>
         </div>
     </header>
-
-    
-         '<nav>
-            <div class="navbar navbar-expand-lg bg-body-tertiary">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="pageAccueil.php"><b>
-                            <h1><b>Menu principal</b></h1>
-                        </b></a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav">
-                            <li class="nav-item"><a class="nav-link active" aria-current="page" href="pageListeAlbums.php"><b>&nbsp;&nbsp;&nbsp;Liste des albums disponibles</b></a></li>
-                            <?php
+    <nav>
+        <div class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="pageAccueil.php"><b>
+                        <h1><b>Menu principal</b></h1>
+                    </b></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="pageListeAlbums.php"><b>&nbsp;&nbsp;&nbsp;Liste des albums disponibles</b></a></li>
+                        <?php
                         if (isset($_SESSION['role'])) {
                             if ($_SESSION['role'] == 'CLIENT') {
                                 echo '<li class="nav-item"><a class="nav-link" href="pageAchat.php"><b>&nbsp;&nbsp;&nbsp;Passer un achat</b></a></li>';
@@ -62,12 +59,10 @@
                             }
                         }
                         ?>
-                    </div>
                 </div>
             </div>
-        </nav>
-    
-
+        </div>
+    </nav>
 
     <?php
     function getPageNom($page)
--- Nicolas_Plouffe_William_Crepault_TP2_Web_2/pageAchat.php
+++ Nicolas_Plouffe_William_Crepault_TP2_Web_2/pageAchat.php
@@ -40,16 +40,38 @@ try {
             <?php include("boutonsConDecon.infos.php"); ?>
         </div>
         <div class="row py-4">
-            <div class="col-sm-12 col-lg-6">
+            <div class="col-sm-12 col-lg-8">
                 <div class="custom-border px-2">
                     <h3>Oeuvres disponibles:</h3>
+                    <div>
+                        <ul class="list-unstyled">
+                            <?php
+                            $product_array = chercherOeuvre("-1");
+                            if (!empty($product_array)) {
+                                foreach ($product_array as $oeuvre) {
+                                    echo '<div class="row py-3">';
+                                    echo '<div class="col-sm-12 col-md-4 col-lg-2"><span><b>Code album: </b></span>' . $oeuvre->getCodeAlbum() . '</div>';
+                                    echo '<div class="col-sm-12 col-md-4 col-lg-2"><span><b>Titre: </b></span>' . $oeuvre->getTitreOeuvre() . '</div>';
+                                    echo '<div class="col-sm-12 col-md-4 col-lg-2"><span><b>Prix: </b></span>' . $oeuvre->getPrix() . '<span>$</span></div>';
+                                    echo '<div class="col-sm-12 col-md-12 col-lg-6">';
+                                    echo '<form action="pageAchat.php?action=add&id_oeuvre=' . $oeuvre->getIdOeuvre() . '" method="post" class="d-flex align-items-center">';
+                                    echo '<input type="number" name="quantity" value="1" min="1" max="10" class="form-control mr-2" style="width: 100px;">';
+                                    echo '<button type="button" id="panier" class="styled-button"><img src="Images/retirer_panier.png" alt="retirer_panier"> vider panier</button>';
+                                    echo '</form>';
+                                    echo '</div>';
+                                    echo '</div>';
+                                    echo '</li>';
+                                }
+                            }
+                            ?>
+                        </ul>
+                    </div>
                 </div>
-
             </div>
-            <div class="col-sm-12 col-lg-6">
+            <div class="col-sm-12 col-lg-4">
                 <div class="custom-border px-2">
                     <h3 id="menu">Voir mon panier:&nbsp;&nbsp;</h3>
-                    <div class="col-sm-12 d-flex justify-content-end px-4 py-2">
+                    <div class="col-sm-12 d-flex justify-content-center px-4 py-2">
                         <?php
                         if (isset($_SESSION["cart_item"])) {
                             $total_quantity = 0;
@@ -63,12 +85,12 @@ try {
                                      alt="panier">&nbsp;&nbsp;' . $qt . ' articles</button>';
                             }
                         } else
-                        echo '<button type="button" id="panier" class="styled-button"><img src="Images/panier.png" 
+                            echo '<button type="button" id="panier" class="styled-button"><img src="Images/panier.png" 
                         alt="panier">&nbsp;&nbsp; article</button>';
                         ?>
                         <?php
                         $qt = 1;
-                        
+
                         ?>
                     </div>
                 </div>
