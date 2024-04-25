69e3555832d9a00407ab59f5d5d36cf159d60576
William Crépault <william.crepault@gmail.com>
2024-04-25 14:26:35 -0400

    ajustement affichage boutons dé/connexions

--- Nicolas_Plouffe_William_Crepault_TP2_Web_2/boutonsConDecon.infos.php
+++ Nicolas_Plouffe_William_Crepault_TP2_Web_2/boutonsConDecon.infos.php
@@ -1,4 +1,3 @@
-
 <?php
 
 if ($pageNom == 'pageAuth') {
@@ -12,23 +11,42 @@ if ($pageNom == 'pageAuth') {
 
     if (isset($_SESSION['role'])) {
         if ($_SESSION['role'] == 'CLIENT' || $_SESSION['role'] == 'ADMIN') {
-            echo '<form method="post"><button type="submit" name="logout" class="styled-button">Déconnexion</button></form>';
-        } 
-        elseif ($_SESSION['role'] == 'GERANT' && $pageNom == 'pageAjoutUtilisateur'){
-            echo '<form method="post"><button type="submit" name="logout" class="styled-button">Déconnexion</button></form>';
+    ?>
+            <div class="d-flex justify-content-end mb-3">
+                <form method="post"><button type="submit" name="logout" class="styled-button">Déconnexion</button></form>
+            </div>
+        <?php
+        } elseif ($_SESSION['role'] == 'GERANT' && $pageNom == 'pageAjoutUtilisateur') {
+        ?>
+            <div class="d-flex justify-content-end mb-3">
+                <form method="post"><button type="submit" name="logout" class="styled-button">Déconnexion</button></form>
+            </div>
+        <?php
+        } elseif ($_SESSION['role'] == 'GERANT') {
+        ?>
+            <div class="d-flex justify-content-end mb-3">
+                <button type="button" id="ajoutUtilisateur" class="styled-button">Ajout utilisateur</button>
+            </div>
+            <div class="d-flex justify-content-end mb-3">
+                <form method="post"><button type="submit" name="logout" class="styled-button">Déconnexion</button></form>
+            </div>
+        <?php
         }
-        elseif ($_SESSION['role'] == 'GERANT') {
-            echo '<button type="button" id="ajoutUtilisateur" class="styled-button">Ajout utilisateur</button>';
-            echo '<form method="post"><button type="submit" name="logout" class="styled-button">Déconnexion</button></form>';
-        }
-        
-    } 
-    elseif ($pageNom == 'pageAjoutUtilisateur') {
-        echo '<button type="button" id="connexion" class="styled-button">Connexion</button>';
-    } 
-    else {
-        echo '<button type="button" id="enregistrer" class="styled-button">S\'enregistrer</button>';
-        echo '<button type="button" id="connexion" class="styled-button">Connexion</button>';
+    } elseif ($pageNom == 'pageAjoutUtilisateur') {
+        ?>
+        <div class="d-flex justify-content-end mb-3">
+            <button type="button" id="connexion" class="styled-button">Connexion</button>
+        </div>
+    <?php
+    } else {
+    ?>
+        <div class="d-flex justify-content-end mb-3">
+            <button type="button" id="enregistrer" class="styled-button">S'enregistrer</button>
+        </div>
+        <div class="d-flex justify-content-end mb-3">
+            <button type="button" id="connexion" class="styled-button">Connexion</button>
+        </div>
+<?php
     }
 }
 ?>
