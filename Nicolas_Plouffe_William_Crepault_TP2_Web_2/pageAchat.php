4ece4b4a9968b9b93af69335dda636793e37656c
William Crépault <william.crepault@gmail.com>
2024-04-26 15:34:48 -0400

    modification syntaxe code + majuscule au bouton Article(s)

--- Nicolas_Plouffe_William_Crepault_TP2_Web_2/pageAchat.php
+++ Nicolas_Plouffe_William_Crepault_TP2_Web_2/pageAchat.php
@@ -7,13 +7,13 @@ require_once "fichiersPHP/Panier.class.php";
 $res = "";
 
 try {
-    
+
     if (!empty($_GET["action"])) {
-        
+
         switch ($_GET["action"]) {
             case "add":
                 if (!empty($_POST["quantite"])) {
-                    
+
                     $item = chercherOeuvre($_GET["id_oeuvre"])[0];
                     if (isset($_SESSION["panier_oeuvre"]) && !empty($_SESSION["panier_oeuvre"])) {
                         $monpanier = unserialize($_SESSION["panier_oeuvre"]);
@@ -52,23 +52,23 @@ try {
                         if (!empty($product_array)) {
                             foreach ($product_array as $oeuvre) {
                         ?>
-                            <div class="row py-2 align-items-center">
-                                <div class="col-sm-12 col-md-4 col-lg-2 d-flex justify-content-center">
-                                    <img src="Images/<?php echo $oeuvre->getAlbumImg() ?>" alt="<?php echo $oeuvre->getAlbumImg() ?>" class="resize img-fluid rounded">
+                                <div class="row py-2 align-items-center">
+                                    <div class="col-sm-12 col-md-4 col-lg-2 d-flex justify-content-center">
+                                        <img src="Images/<?php echo $oeuvre->getAlbumImg() ?>" alt="<?php echo $oeuvre->getAlbumImg() ?>" class="resize img-fluid rounded">
+                                    </div>
+                                    <div class="col-sm-12 col-md-4 col-lg-4 d-flex justify-content-center">
+                                        <p><b>Titre:&nbsp;</b><?php echo $oeuvre->getTitreOeuvre() ?></p>
+                                    </div>
+                                    <div class="col-sm-12 col-md-4 col-lg-2 d-flex justify-content-center px-0">
+                                        <p><b>Prix:&nbsp;</b><?php echo $oeuvre->getPrix() ?>.00$</p>
+                                    </div>
+                                    <div class="col-sm-12 col-md-12 col-lg-4 d-flex justify-content-center">
+                                        <form action="pageAchat.php?action=add&id_oeuvre=<?php echo $oeuvre->getIdOeuvre() ?>" method="post" class="d-flex align-items-center">
+                                            <input type="number" name="quantite" value="1" min="1" max="10" class="form-control" style="width: 75px; margin-right: 25px;">
+                                            <button type="submit" id="ajoutPanier" class="styled-button" style="width: 140px;"><img src="Images/ajout_panier.png" alt="ajouter panier"> Ajouter</button>
+                                        </form>
+                                    </div>
                                 </div>
-                                <div class="col-sm-12 col-md-4 col-lg-4 d-flex justify-content-center">
-                                    <p><b>Titre:&nbsp;</b><?php echo $oeuvre->getTitreOeuvre() ?></p>
-                                </div>
-                                <div class="col-sm-12 col-md-4 col-lg-2 d-flex justify-content-center px-0">
-                                    <p><b>Prix:&nbsp;</b><?php echo $oeuvre->getPrix() ?>.00$</p>
-                                </div>
-                                <div class="col-sm-12 col-md-12 col-lg-4 d-flex justify-content-center">
-                                <form action="pageAchat.php?action=add&id_oeuvre=<?php echo $oeuvre->getIdOeuvre() ?>" method="post" class="d-flex align-items-center">
-                                    <input type="number" name="quantite" value="1" min="1" max="10" class="form-control" style="width: 75px; margin-right: 25px;">
-                                    <button type="submit" id="ajoutPanier" class="styled-button" style="width: 140px;"><img src="Images/ajout_panier.png" alt="ajouter panier"> Ajouter</button>
-                                </form>
-                            </div>
-                        </div>
                         <?php }} ?>
                     </div>
                 </div>
@@ -84,19 +84,16 @@ try {
 
                             if (!empty($_SESSION["panier_oeuvre"])) {
                                 $monpanier = unserialize($_SESSION['panier_oeuvre']);
-
                                 $qt = $monpanier->getCountItems();
-                                echo '<button type="button" id="panier" class="styled-button"><img src="Images/panier.png" 
-                                     alt="panier">&nbsp;&nbsp;' . $qt . ' Articles</button>';
-                            }
-                        } else
-                            echo '<button type="button" id="panier" class="styled-button"><img src="Images/panier.png" 
-                            alt="panier">&nbsp;&nbsp; Article</button>';
                         ?>
+                                <button type="button" id="panier" class="styled-button"><img src="Images/panier.png" alt="panier">&nbsp;&nbsp;<?= $qt ?> Articles</button>
+                            <?php }} else { ?>
+                            <button type="button" id="panier" class="styled-button"><img src="Images/panier.png" alt="panier">&nbsp;&nbsp; Article</button>
+                        <?php } ?>
                     </div>
                 </div>
             </div>
         </div>
-    </div>    
+    </div>
 </main>
 <?php include("foots.infos.php"); ?>
