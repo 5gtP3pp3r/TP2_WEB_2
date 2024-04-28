1ad3c8f8a2bd7a61bc2218f1b4e04dfedbdb5319
William Crépault <william.crepault@gmail.com>
2024-04-28 11:22:09 -0400

    changement de nom fichier

---
+++ Nicolas_Plouffe_William_Crepault_TP2_Web_2/fichiersPHP/validerAlbum.php
@@ -0,0 +1,54 @@
+<?php
+require_once 'fichiersPHP/connexionBD.php';
+
+function validerAlbum() {
+
+    $errors = [];
+
+    if (empty($_POST['titre'])) {
+        $errors[] = "Le titre est requis.";
+    }
+    elseif (!preg_match("/^[a-z0-9à-öø-ÿ]+(?:[ \-_.]*[a-z0-9à-öø-ÿ]+)*$/i", $_POST['titre'])) {
+        $errors[] = 'Le titre doit contenir au moins 1 caractère,accèpte les accents, les espaces, ".","-" et les "_" ';
+    }
+    
+    if (empty($_POST['code'])){
+        $errors[] = "Le code album est requis.";
+    }
+    elseif (!preg_match("/^[A-Z]{3}[0-9]{4}$/", $_POST['code'])) {
+        $errors[] = "Le code doit contenir 3 lettres majuscule suivit de 4 chiffres.";
+    }
+
+    if (empty($_POST['dateAjout']) || $_POST['dateAjout'] != date("Y-m-d")){
+        $errors[] = "La date d'aujourd'hui est requise.";
+    }
+
+    if (empty($_POST['genreMusical']) || $_POST['genreMusical'] == 0) {
+        $errors[] = "Vous devez choisir un genre.";
+    }
+
+    if (empty($_POST['photo'])) {
+        $errors[] = "Le nom de la photo est requis";
+    }
+
+    if (count($errors) === 0) {
+
+        $titre = $_POST['titre'];
+        $code = $_POST['code'];
+        $dateAjout = $_POST['dateAjout'];
+        $genreMusical = $_POST['genreMusical'];
+        $photo = $_POST['photo'];
+
+        $resultat = ajouterAlbumBD($titre, $code, $dateAjout, $genreMusical, $photo);
+
+        if ($resultat) {
+            return "Album ajouté avec succès !";
+        } 
+        else {
+            return ["Erreur lors de l'ajout de l'album dans la base de données."];
+        }
+    } 
+    else {
+        return $errors;
+    }
+}
---
+++ Nicolas_Plouffe_William_Crepault_TP2_Web_2/fichiersPHP/validerArtistes.php
@@ -0,0 +1,85 @@
+<?php
+require_once("connexionBD.php");
+
+
+/*function validerChampsNom()
+{
+    $regexNom = "/^[a-zàâçéèêëîïôûùüÿñæœ0-9 .'-]*$/i";
+    $estValide = false;
+
+    if (!empty($_POST['idArtiste']) && isset($_POST['idArtiste']) && preg_match($regexNom, $_POST['idArtiste'])) {
+        $estValide = true;
+    }
+
+    return $estValide;
+}
+
+
+function validerChampsPhoto()
+{
+
+    $regexImage = "/^[^\s]+\.(jpg|jpeg|png|gif|bmp)$/";
+    $estValide = false;
+
+    if (!empty($_POST['photoArtiste']) && isset($_POST['photoArtiste']) && preg_match($regexImage, $_POST['photoArtiste'])) {
+        $estValide = true;
+    }
+
+    return $estValide;
+}
+
+function validerDesChamps()
+{ {
+        return validerChampsNom() && validerChampsPhoto();
+    }
+}
+
+
+// Modified $SELECTION_PLACEHOLDER$ code
+
+function AfficherMenuVille()
+{
+
+    $pdo = connexionBD(); // Connexion à la base de données
+
+    $sql = "SELECT * FROM villes"; // Requête SQL
+
+    $pdoResultantBrute = $pdo->query($sql); // Exécution de la requête
+
+    $ppoResultantDeployee = $pdoResultantBrute->fetchAll(); // Récupération des données
+
+    foreach ($ppoResultantDeployee as $col => $colVal) {
+        echo '<option value="' . $colVal["id"] . '">' . $colVal["nom_ville"] . '</option>';
+    }
+    $pdo = null; // Fermeture de la connexion
+
+}
+*/
+
+
+function validerArtiste()
+{
+
+    $conn = connexionBD();
+    try {
+        $sql = "INSERT INTO artistes (nom_artiste, pht_artiste, id_ville) VALUES (:nomArtiste, :photoArtiste, :ville )";
+
+        $stmt = $conn->prepare($sql); // Préparation de la requête
+
+        $stmt->bindParam(':nomArtiste', $_POST['idArtiste']);
+        $stmt->bindParam(':photoArtiste', $_POST['photoArtiste']);
+        $stmt->bindParam(':ville', $_POST['ville']);
+
+        $stmt->execute();
+        $conn = null; // Fermeture de la connexion
+    } catch (PDOException $e) {
+        error_log("erreur ajout artiste: " . $e->getMessage());
+        return false;
+    }
+}
+
+function testConnection()
+{
+
+    echo "test";
+}
