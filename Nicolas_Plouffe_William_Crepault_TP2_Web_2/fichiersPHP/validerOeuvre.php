5ad3a00edb1fd99c9907edc864e5d20320bff22f
William Crépault <william.crepault@gmail.com>
2024-05-02 14:30:15 -0400

    mode var errors pour erreurs

--- Nicolas_Plouffe_William_Crepault_TP2_Web_2/fichiersPHP/validerOeuvre.php
+++ Nicolas_Plouffe_William_Crepault_TP2_Web_2/fichiersPHP/validerOeuvre.php
@@ -11,59 +11,55 @@ function validerOeuvre()
     $ValeurMax = 10;
     $regexLyrics = "/^[a-zA-Z0-9 ]+$/";
 
-
-    $errors = [];
+    $erreurs = [];
     
-
     if (empty($_POST['titrePiece'])) {
-        $errors[] = "Le titre de l'oeuvre est requis.";
+        $erreurs[] = "Le titre de l'oeuvre est requis.";
     } else if (!preg_match($regExNom, $_POST['titrePiece'])) {
-        $errors[] = 'Le titre doit contenir que des lettres, des chiffres et des espaces';
+        $erreurs[] = 'Le titre doit contenir que des lettres, des chiffres et des espaces';
     }
 
     if ($_POST['nomArtiste'] == 0) {
-        $errors[] = "Choisir un artiste parmis la liste.";
+        $erreurs[] = "Choisir un artiste parmis la liste.";
     }
 
     if ($_POST['role'] == 0) {
-        $errors[] = "Choisir un rôle parmis la liste.";
+        $erreurs[] = "Choisir un rôle parmis la liste.";
     }
 
     if (empty($_POST['duree']) || $_POST['duree'] <= $ZERO) {
-        $errors[] = "La durée est requise.";
+        $erreurs[] = "La durée est requise.";
     } else if ($_POST['duree'] > $Max) {
-        $errors[] = "La durée ne doit pas dépasser 999 secondes.";
+        $erreurs[] = "La durée ne doit pas dépasser 999 secondes.";
     }
 
     if (!empty($_POST['taille']) && $_POST['taille'] <= $ZERO) {
-        $errors[] = 'La valeur doit être entre 1 et "$DureeMax" mb.';
+        $erreurs[] = 'La valeur doit être entre 1 et "$DureeMax" mb.';
     } else if (!empty($_POST['taille']) && $_POST['taille'] > $Max) {
-        $errors[] = "La taille ne doit pas dépasser 999 mb.";
+        $erreurs[] = "La taille ne doit pas dépasser 999 mb.";
     }
 
     if (empty($_POST['prix']) || $_POST['prix'] < $ValeurMin) {
-        $errors[] = 'La valeur requise doit être suppérieure à "$ZERO" $.';
+        $erreurs[] = 'La valeur requise doit être suppérieure à "$ZERO" $.';
     } else if ($_POST['prix'] > $ValeurMax) {
-        $errors[] = 'La valeur ne doit pas dépasser " $ValeurMax" $.';
+        $erreurs[] = 'La valeur ne doit pas dépasser " $ValeurMax" $.';
     }
 
     if (empty($_POST['datePublication'])) {
-        $errors[] = "La date d'aujourd'hui est requise.";
+        $erreurs[] = "La date d'aujourd'hui est requise.";
     } else if ($_POST['datePublication'] > date('Y-m-d')) {
-        $errors[] = "La date ne doit pas être supérieure à la date d'aujourd'hui.";
+        $erreurs[] = "La date ne doit pas être supérieure à la date d'aujourd'hui.";
     }
 
     if ($_POST['codeAlbum'] == 0) {
-        $errors[] = "Choisir un code d'album parmis la liste.";
+        $erreurs[] = "Choisir un code d'album parmis la liste.";
     }    
 
     if (!empty($_POST['lyrics']) && !preg_match($regexLyrics, $_POST['lyrics'])) {
-        $errors[] = "Les paroles ne doivent contenir que des lettres, des chiffres et des espaces.";
+        $erreurs[] = "Les paroles ne doivent contenir que des lettres, des chiffres et des espaces.";
     }
 
-    // AJOUTER VALIDATION EMAIL UNIQUE
-
-    if (count($errors) == 0) {
+    if (count($erreurs) == 0) {
 
         $titrePiece = $_POST['titrePiece'];
         $nomArtiste = $_POST['nomArtiste'];
@@ -82,6 +78,6 @@ function validerOeuvre()
             return ["Erreur lors de l'ajout de l'oeuvre dans la base de données."];
         }
     } else {
-        return $errors;
+        return $erreurs;
     }
 }
