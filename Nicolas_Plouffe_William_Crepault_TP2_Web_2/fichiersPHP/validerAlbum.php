2b548a05de159044106bbdfc7245699966c8fa26
William Crépault <william.crepault@gmail.com>
2024-05-02 14:32:48 -0400

    mode var errors pour erreurs

--- Nicolas_Plouffe_William_Crepault_TP2_Web_2/fichiersPHP/validerAlbum.php
+++ Nicolas_Plouffe_William_Crepault_TP2_Web_2/fichiersPHP/validerAlbum.php
@@ -5,40 +5,40 @@ function validerAlbum()
 {
     date_default_timezone_set('Etc/GMT+4');
 
-    $errors = [];
+    $erreurs = [];
 
     if (empty($_POST['titre'])) {
-        $errors[] = "Le titre est requis.";
+        $erreurs[] = "Le titre est requis.";
     } elseif (!preg_match("/^[a-z0-9à-öø-ÿ]+(?:[ \-_.]*[a-z0-9à-öø-ÿ]+)*$/i", $_POST['titre'])) {
-        $errors[] = 'Le titre doit contenir au moins 1 caractère,accèpte les accents, les espaces, ".","-" et les "_" ';
+        $erreurs[] = 'Le titre doit contenir au moins 1 caractère,accèpte les accents, les espaces, ".","-" et les "_" ';
     }
 
     if (empty($_POST['code'])) {
-        $errors[] = "Le code album est requis.";
+        $erreurs[] = "Le code album est requis.";
     } elseif (!preg_match("/^[A-Z]{3}[0-9]{4}$/", $_POST['code'])) {
-        $errors[] = "Le code doit contenir 3 lettres majuscule suivit de 4 chiffres.";
+        $erreurs[] = "Le code doit contenir 3 lettres majuscule suivit de 4 chiffres.";
     }
 
     if (empty($_POST['dateAjout']) || $_POST['dateAjout'] != date("Y-m-d")) {
-        $errors[] = "La date d'aujourd'hui est requise.";
+        $erreurs[] = "La date d'aujourd'hui est requise.";
     }
 
     if (empty($_POST['genreMusical']) || $_POST['genreMusical'] == 0) {
-        $errors[] = "Vous devez choisir un genre.";
+        $erreurs[] = "Vous devez choisir un genre.";
     }
 
     if (!empty($_POST['photo']) && !preg_match("/^[^\s]+\.(jpg|jpeg|png|gif|bmp)$/i", $_POST['photo'])) {
-        $errors[] = "Le nom de la photo est requis";
+        $erreurs[] = "Le nom de la photo est requis";
     }
 
     $codeAlbumArray = chercherCodeAlbum();
     $codeEntrant = $_POST['code'];
 
     if (in_array($codeEntrant, $codeAlbumArray)) {
-        $errors[] = "Code album existant";
+        $erreurs[] = "Code album existant";
     }
 
-    if (count($errors) === 0) {
+    if (count($erreurs) === 0) {
 
         $titre = $_POST['titre'];
         $code = $_POST['code'];
@@ -53,21 +53,21 @@ function validerAlbum()
             $cheminImage = $dossierImages . $photo;
 
             if (!file_exists($cheminImage)) {
-                $errors[] = 'L\'image n\'existe pas dans le dossier "Images"';
+                $erreurs[] = 'L\'image n\'existe pas dans le dossier "Images"';
             }
         }
-        if (empty($errors)) {
+        if (empty($erreurs)) {
             $resultat = ajouterAlbumBD($titre, $code, $dateAjout, $genreMusical, $photo);
             if ($resultat) {
                 return "Album ajouté avec succès !";
             } else {
-                $errors[] = "Erreur lors de l'ajout de l'album dans la base de données.";
-                return $errors;
+                $erreurs[] = "Erreur lors de l'ajout de l'album dans la base de données.";
+                return $erreurs;
             }
         } else {
-            return $errors; 
+            return $erreurs; 
         }
     } else {
-        return $errors;  
+        return $erreurs;  
     }
 }
