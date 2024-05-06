976c1180ba424ec5fe7fead12ec9ac62218b4254
William Crépault <william.crepault@gmail.com>
2024-05-06 17:16:15 -0400

    mise à jour

--- Nicolas_Plouffe_William_Crepault_TP2_Web_2/index.php
+++ Nicolas_Plouffe_William_Crepault_TP2_Web_2/index.php
@@ -4,9 +4,6 @@ session_start();
 if (session_status() === PHP_SESSION_ACTIVE) {
     session_unset(); 
     session_destroy(); 
-    session_write_close(); 
-    setcookie(session_name(),'',0,'/'); 
-    session_regenerate_id(true); 
 }
     header("Location: pageAccueil.php");
 ?>
