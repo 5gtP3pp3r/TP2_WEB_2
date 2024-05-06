ff6d90e1ed1da6b4d78bc0b9360a3f94a78b0cd2
William Crépault <william.crepault@gmail.com>
2024-05-06 17:12:36 -0400

    ajout destruction session dans index

--- Nicolas_Plouffe_William_Crepault_TP2_Web_2/index.php
+++ Nicolas_Plouffe_William_Crepault_TP2_Web_2/index.php
@@ -1,3 +1,19 @@
 <?php
+session_start();
+
+if (session_status() === PHP_SESSION_ACTIVE) {
+    session_unset(); 
+    session_destroy(); 
+    session_write_close(); 
+    setcookie(session_name(),'',0,'/'); 
+    session_regenerate_id(true); 
+}
     header("Location: pageAccueil.php");
 ?>
+
+
+<!-- 
+La page index détruit toutes les sessions en court! 
+Par contre si on essait d'accéder à une page via l'url et que 
+l'utilisateur n'a pas le bon rôle, il ne sera pas redirigé ici, 
+mais à la pageAccueil.php pour ne pas fermer sa session.    -->
