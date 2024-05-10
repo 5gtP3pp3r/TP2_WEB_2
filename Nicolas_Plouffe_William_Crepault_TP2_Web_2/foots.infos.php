2fc29e917ab8192a899d86e9cd7553c7bb56638b
William Crépault <william.crepault@gmail.com>
2024-05-10 16:05:06 -0400

    modification script et switch/case pour mettre l,acre .js juste sur les page qui en ont besoin (achat et panier)

--- Nicolas_Plouffe_William_Crepault_TP2_Web_2/foots.infos.php
+++ Nicolas_Plouffe_William_Crepault_TP2_Web_2/foots.infos.php
@@ -39,15 +39,18 @@
                 return '<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>' .
                        '<script src="java_script/date.js"></script>' . 
                        '<script src="java_script/validationAlbumjQuery.js"></script>';
+            case 'pageAchat':
+                return '<script src="java_script/ancrePage.js"></script>';
+            case 'pagePanier':
+                return '<script src="java_script/ancrePage.js"></script>';
         }        
     }   
 ?>
-<script src="java_script/ancrePage.js"></script>
 <script src="java_script/clicksRedirections.js"></script>
+<?= $pageScript ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" 
 integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" 
 crossorigin="anonymous"></script>
-<?= $pageScript ?>
 </body>
 
 </html>
